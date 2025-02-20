f600a3b60f195c5cc7544ba10cd90fbb98117492
Ryan Appel <rdappel@gmail.com>
2025-02-19 19:27:18 -0600

    bug fix

--- site.js
+++ site.js
@@ -31,32 +31,34 @@ const angleToImageIndex = angle => {
     return ((Math.round((angle + Math.PI) / (Math.PI / 4)) + 7) % 8)
 }
 
+const hasHoverState = e => e.parentElement.querySelector(':hover') === e
 
 const instructorSection = document.querySelector('#instructors')
 instructorSection.addEventListener('mousemove', e => {
-	// mouse position
 	const x = e.clientX
 	const y = e.clientY
 
 	Object.entries(instructorImages).forEach(([instructor, images]) => {
-		// calculate offset from center of image to mouse
+		// check if instructor has hover state
+		const div = document.querySelector(`#${instructor}`)
+		if (!div) return
+
+		const hover = hasHoverState(div)
+		if (hover) {
+			const image = images.default
+			img.src = image.src
+			img.classList.add('hover')
+			return
+		}
+
 		const img = instructorSection.querySelector(`#${instructor} img`)
 		const offsetX = x - img.offsetLeft - halfImageSize.width
 		const offsetY = y - img.offsetTop - halfImageSize.height
 		const angle = Math.atan2(offsetY, offsetX)
-		const distanceSq = offsetX * offsetX + offsetY * offsetY
-		const radiusSq = 90 * 90
-		if (distanceSq <= radiusSq) {
-			const image = images.default
-			img.src = image.src
-			img.classList.add('hover')
-		}
-		else {
-			const index = angleToImageIndex(angle)
-			const image = images[index.toString()]
-			img.src = image.src
-			img.classList.remove('hover')
-		}
+		const index = angleToImageIndex(angle)
+		const image = images[index.toString()]
+		img.src = image.src
+		img.classList.remove('hover')
 
 	})
 })
